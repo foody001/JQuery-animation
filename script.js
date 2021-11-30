85d75a608ec45d755f7c7a180690624750eee49e
foody001 <94222968+foody001@users.noreply.github.com>
2021-11-30 10:05:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$ (function(){
+$("#start").click(function(){
+$('div').animate({  left: '600px',}, 3000);
+$('div').animate({  opacity: 0.2,}, 3000);
+$('div').animate({  width: '200px',}, 3000);
+$('div').animate({  height: '200px',}, 3000);
+});
+$("#stop").click(function(){
+$("div").stop(true);
+});
+
+ });
